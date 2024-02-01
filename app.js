2014f36e0810e230345c9f3fdb8fa53be97a79cd
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-02-02 01:09:13 +0530

    Responding to URL parameters

--- app.js
+++ app.js
@@ -21,9 +21,49 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  //after ? can be used to do optional routing, if not found it will display undefined
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Done');
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body); // req.body.id = newId but we do not want to mutate the original body object
+  //allows us to create new object by merging two existing object.
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  // res.send('Done'); cannot sent two res at a time
 });
 const port = 3000;
 
