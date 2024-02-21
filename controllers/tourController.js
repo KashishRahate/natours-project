dd3fc1508bc1fb3d79230df0fe5636562f632e5b
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-02-22 01:44:10 +0530

    Added Filtering in API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,29 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // Build Query
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+    // console.log(req.query, queryObj);
+
+    // const tours = await Tour.find(queryObj);
+    const query = Tour.find(queryObj);
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy'); //lte or lt
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       result: tours.length,
