9a02f775aa578d906feb788477bca91e80f1716f
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-13 00:04:06 +0530

    Signing up Users using JWT tokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,28 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // const newUser = await User.create(req.body); // here anyone can specify the role as admin -- very serious security flaw
+  const newUser = await User.create({
+    // here role is not stored, we only allow data that we actually need to be put into the new user(email, password), manually enter data is not stored for the new user
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign(
+    { id: newUser._id } /*Payload*/,
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+  );
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
