4cfb882b7814e73b4c6aed1215b0e8cde562d5cb
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-14 01:53:09 +0530

    Protecting tour route by header tokenisation

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     // here role is not stored, we only allow data that we actually need to be put into the new user(email, password), manually enter data is not stored for the new user
     name: req.body.name,
     email: req.body.email,
-    password: req.ubody.password,
+    password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
 
@@ -51,4 +51,27 @@ exports.login = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1) Getting token and check of it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  console.log(token);
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401),
+    );
+  }
+  // 2) Verification token
+
+  // 3) Check if user still exists
+
+  // 4) Check if user changed password after the token was issued
+  next();
 });
