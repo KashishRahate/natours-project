be8e5ab674f7194d29ee9858d0caea96ef151584
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-16 17:05:57 +0530

    Setting security HTTP headers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -10,18 +11,26 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 console.log(process.env.NODE_ENV);
 // 1) GLOBAL MiddleWares
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
-  max: 3,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
 
-app.use(express.json()); //express.json() is a middleware(function that can modify the incoming request data, it stands b/w request and response)
+//  Body Parserm reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); //express.json() is a middleware(function that can modify the incoming request data, it stands b/w request and response)
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
@@ -29,6 +38,7 @@ app.use(express.static(`${__dirname}/public`));
 //   next();
 // });
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
