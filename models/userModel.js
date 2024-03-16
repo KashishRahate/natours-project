b77c6474aa6ec4fc281bb318f3fcc1e45e80daee
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-16 16:46:46 +0530

    Updated and deleted current user data, also send JWT via cookie

--- models/userModel.js
+++ models/userModel.js
@@ -44,6 +44,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 
 userSchema.pre('save', async function (next) {
@@ -64,6 +69,11 @@ userSchema.pre('save', function (next) {
   next();
 });
 
+userSchema.pre(/^find/, function (next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
+  next();
+});
 // Instance method
 userSchema.methods.correctPassword = async function (
   candidatePassword,
