134ed5d4c85cec5e89c9ecfd23fdf2ccd9e0a1aa
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-16 17:47:15 +0530

    Prevented Prarameter Pollution

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const hpp = require('hpp');
 
 const app = express();
 console.log(process.env.NODE_ENV);
@@ -29,6 +32,26 @@ app.use('/api', limiter);
 
 //  Body Parserm reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); //express.json() is a middleware(function that can modify the incoming request data, it stands b/w request and response)
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS(Cross site scripting)
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
