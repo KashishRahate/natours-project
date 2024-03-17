4cd8e5dc32a65c70aa045f27cddd705e3bf72b03
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-18 00:47:01 +0530

    Modelling review parent referencing

---
+++ models/reviewModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Write your review, it cannot be empty'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true }, // when we have virtual property(a field that is not stored in a database)
+    toObject: { virtuals: true }, // but calculated using some other value, we want this to show up whenever there is a o/p
+  },
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
+// Parent Referencing
