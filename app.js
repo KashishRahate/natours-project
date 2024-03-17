c1abf21adad16d85e941d381698856e6502d572f
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-18 01:35:09 +0530

    Creating and Getting Reviews

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 const hpp = require('hpp');
 
 const app = express();
@@ -71,6 +72,7 @@ app.use((req, res, next) => {
 // 3) Routes
 app.use('/api/v1/tours', tourRouter); //mounting of the router
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   // res.status(404).json({
