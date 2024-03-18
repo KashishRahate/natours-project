301aa3706e8c26305e1a94ce0b538f836742edd9
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-19 01:23:57 +0530

    Build handler factory functions for  update and create

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -17,20 +17,14 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
   // Allow nested routes
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
 
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview,
-    },
-  });
-});
+exports.createReview = factory.createOne(Review);
 
 exports.getReview = catchAsync(async (req, res, next) => {
   const review = await Review.findById(req.params.id);
@@ -45,5 +39,7 @@ exports.getReview = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.updateReview = factory.updateOne(Review);
 
 exports.deleteReview = factory.deleteOne(Review);
