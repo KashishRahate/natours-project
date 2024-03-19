1fba3979de6563cea842c6ee9a4fd6ea8f13f4bb
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-19 23:21:27 +0530

    Added missing authentication and authorization

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -6,11 +6,12 @@ const router = express.Router({ mergeParams: true });
 
 // POST /tour/3245t4gt45/reviews
 // POST /reviews
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview,
@@ -19,8 +20,14 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview,
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview,
+  );
 
 router.route('/:id').get(reviewController.getReview);
 
