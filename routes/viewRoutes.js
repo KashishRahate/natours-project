35375e099925d0c0ae7dacdd89b6fe8259e4d95b
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-25 02:33:55 +0530

    Built the UI of Tour Page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,6 +3,5 @@ const viewsController = require('../controllers/viewsController');
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
-router.get('/tour', viewsController.getTour);
-
+router.get('/tours/:slug', viewsController.getTour);
 module.exports = router;
