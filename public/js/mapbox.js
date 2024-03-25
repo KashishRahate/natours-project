5e7b2d2c3609201df99ca27214fdbc8dcc4a0e3b
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-25 17:23:59 +0530

    Included a Map with Mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,50 @@
 /*  eslint-disable */
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoia2FzaGlzaC1yYWhhdGUiLCJhIjoiY2x1NnFlZThhMjMyODJtcGQzOWRweHg5cyJ9.egDl9U4e_IX2bN3Xy7hjqA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/kashish-rahate/clu6u2gbi00eb01p69eh04n90',
+  scrollZoom: false,
+  // center: [-118.3281236, 33.7433727],
+  // zoom: 10,
+  // interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // create Marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
