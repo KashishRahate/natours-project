3ef57bbb2fa1a8d52df09178ed56ba6976e16b2d
gitrider02 <114670210+gitrider02@users.noreply.github.com>
2024-03-26 19:21:16 +0530

    Added more features like Logging out, rendering error pages and built user account page

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
-
-const login = async (email, password) => {
+import axios from 'axios';
+import { showAlert } from './alerts';
+export const login = async (email, password) => {
   console.log(email, password);
   try {
     const res = await axios({
@@ -13,20 +14,25 @@ const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      alert('Logged in successfully!');
+      showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
     // console.log(err.response.data);
   }
 };
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:3000/api/v1/users/logout',
+    });
+    if ((res.data.status = 'success')) location.reload(true);
+  } catch (err) {
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,9 +3,8 @@ const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
 const router = express.Router();
 
-router.use(authController.isLoggedIn);
-
-router.get('/', viewsController.getOverview);
-router.get('/tours/:slug', viewsController.getTour);
-router.get('/login', viewsController.getLoginForm);
+router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/tours/:slug', authController.isLoggedIn, viewsController.getTour);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 module.exports = router;
